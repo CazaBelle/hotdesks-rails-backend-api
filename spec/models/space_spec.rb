dfcd8d104213f9a4bd9d7bc74cd2a1c4d1533f17
Carol Gardiner <carolan.gardiner@gmail.com>
2019-03-28 13:10:56 +0000

    add test to ensure that the Space is valid

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Space, type: :model do
-  it "should habe"
+  it "is valid with attributes" do 
+    expect(Space.new).to be_valid
+  end 
+  it "is valud with a name" do 
+    
+  end 
 end
