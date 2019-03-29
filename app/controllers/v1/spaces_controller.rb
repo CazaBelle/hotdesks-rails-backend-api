d49cc8bcfd58752380407b9d3dfa3c59dee7856d
Carol Gardiner <carolan.gardiner@gmail.com>
2019-03-29 14:53:24 +0000

    TEST: hard coded in values to determine will return the correct json format

--- app/controllers/v1/spaces_controller.rb
+++ app/controllers/v1/spaces_controller.rb
@@ -3,7 +3,8 @@ class V1::SpacesController < ApplicationController
   
   # GET /v1/spaces
   def index
-    spaces = Space.all
+    # spaces = Space.all
+    spaces = {name: 'Club Workspace', address: '31 Clerkenwell Close, Farringdon', city: 'London', postcode: "EC1R 0AT", price: 50}
     render json: spaces
   end
 
